4b50e2dc033a683b109f1422f561e06b205d7296
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-08-04 10:37:35 +0700

    Lecture III - Adding a Named / static Route File

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import PropTypes from 'prop-types'
+
+function AboutPage(props) {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+}
+
+export default AboutPage;
