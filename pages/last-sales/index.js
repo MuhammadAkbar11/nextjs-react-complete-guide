c1a7e283f42686326f55d2d0a30dba748f276e95
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-11-04 13:13:12 +0700

    Lecture V - Using the "useSWR" NextJS Hook

--- pages/last-sales/index.js
+++ pages/last-sales/index.js
@@ -1,50 +1,75 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 function LastSelesPage() {
   const [sales, setSales] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+  const { data, error } = useSWR(
+    "https://nextjs-app-ce56a-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json",
+    fetcher
+  );
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      "https://nextjs-app-ce56a-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json"
-    )
-      .then(response => response.json())
-      .then(data => {
-        const toArr = Object.keys(data).map(key => ({
-          id: key,
-          ...data[key],
-        }));
+    if (data) {
+      const toArr = Object.keys(data).map(key => ({
+        id: key,
+        ...data[key],
+      }));
 
-        setTimeout(() => {
-          setSales(toArr);
-          setIsLoading(false);
-        }, 2000);
-      })
-      .catch(err => {
-        setIsLoading(false);
-        console.log(err);
-      });
-  }, []);
+      setTimeout(() => {
+        setSales(toArr);
+      }, 2000);
+    }
+  }, [data]);
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch(
+  //     "https://nextjs-app-ce56a-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json"
+  //   )
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       const toArr = Object.keys(data).map(key => ({
+  //         id: key,
+  //         ...data[key],
+  //       }));
+
+  //       setTimeout(() => {
+  //         setSales(toArr);
+  //         setIsLoading(false);
+  //       }, 2000);
+  //     })
+  //     .catch(err => {
+  //       setIsLoading(false);
+  //       console.log(err);
+  //     });
+  // }, []);
+
+  if (error) {
+    return <p>failed to load</p>;
+  }
+
+  console.log(data);
+
+  if (!data || !sales) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
-      {isLoading ? (
-        <h4>Loading...</h4>
+      {sales ? (
+        <ul>
+          {sales.map(sale => (
+            <li key={sale.id}>
+              {sale?.username} ({sale?.volume})
+            </li>
+          ))}
+        </ul>
       ) : (
-        <>
-          {sales ? (
-            <ul>
-              {sales.map(sale => (
-                <li key={sale.id}>
-                  {sale?.username} ({sale?.volume})
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>Empty Data</p>
-          )}
-        </>
+        <p>Empty Data.</p>
       )}
     </div>
   );
