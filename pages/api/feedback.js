8363e6c08b7a98214cc862d704fffb91db5365e7
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-04-07 04:15:43 +0700

    Lecture VIII - Parsing the incoming req & executing server-side code

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,7 +1,49 @@
-function handler(req, res) {
-  res.status(200).json({
-    message: "Hello from NextJs Api Route",
+import path from "path";
+import fs from "fs";
+
+const feedbackDataPath = path.join(process.cwd(), "data", "feedback.json");
+
+const saveFeedbackData = data => {
+  const stringifyData = JSON.stringify(data);
+  fs.writeFileSync(feedbackDataPath, stringifyData);
+};
+
+const getFeedbackData = () => {
+  const jsonData = fs.readFileSync(feedbackDataPath);
+  return JSON.parse(jsonData);
+};
+
+function getFeedback(req, res) {
+  res.json({ message: "GET" });
+}
+
+function postFeedback(req, res) {
+  const { email, text } = req.body;
+  const feedbackData = getFeedbackData();
+
+  const newFeedback = {
+    id: String(Object.keys(feedbackData).length + 1),
+    email: email,
+    text: text,
+  };
+
+  feedbackData.push(newFeedback);
+  saveFeedbackData(feedbackData);
+  res.status(201).json({
+    message: "Success send feedback",
+    feedbacks: newFeedback,
   });
+}
+
+function handler(req, res) {
+  const { method } = req;
+
+  const logMethod = {
+    GET: () => getFeedback(req, res),
+    POST: () => postFeedback(req, res),
+  };
+
+  (logMethod[method] || logMethod["DEFAULT"])();
 }
 
 export default handler;
