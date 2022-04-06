15bf0f17f058d5204171bc025abfb392ace179e0
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-04-07 02:59:18 +0700

    Lecture VIII - Preparing the FrontEnd Form

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,40 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const submitFormHandler = event => {
+    event.preventDefault();
+
+    const { value: emailValue } = emailInputRef.current;
+    const { value: feedbackValue } = feedbackInputRef.current;
+
+    console.log(emailValue, feedbackValue);
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form action="" method="POST" onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <br />
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <br />
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <br />
+          <textarea
+            name="feedback"
+            id="feedback"
+            rows="3"
+            ref={feedbackInputRef}
+          ></textarea>
+        </div>
+        <button type="submit">Send Feedback</button>
+      </form>
     </div>
   );
 }
