1becc22cdd3337c22e81355bbbdef7d41d55eb0f
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-12-12 09:45:30 +0700

    make yupErro arrow func to regular js func. then create resAPIError func

--- utils/error.js
+++ utils/error.js
@@ -1,6 +1,10 @@
-export const yupError = error => {
+export function yupError(error) {
   return error.inner.reduce((acc, err) => {
     acc[err.path] = err.message;
     return acc;
   }, {});
-};
+}
+
+export function resAPIError(res, statusCode, error) {
+  return res.status(statusCode).json(error);
+}
