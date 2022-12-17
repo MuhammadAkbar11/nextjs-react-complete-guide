08f278f6101c82f7db37b5612ea4419b136339f4
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-12-18 06:59:32 +0700

    setup react-query

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,39 @@
-import Head from "next/head";
-import Layout from "../components/layout/Layout";
 import "../styles/global.scss";
+import Head from "next/head";
+import { SSRProvider } from "react-bootstrap";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Layout from "../components/layout/Layout";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 1000,
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>BaeEvents</title>
-        <meta
-          name="description"
-          content="Find a lot of great events that allow you to evolve"
-        />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />;
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <SSRProvider>
+        <Layout>
+          <Head>
+            <title>BaeEvents</title>
+            <meta
+              name="description"
+              content="Find a lot of great events that allow you to evolve"
+            />
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
+          </Head>
+          <Component {...pageProps} />;
+        </Layout>
+      </SSRProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
