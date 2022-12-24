a12fabd0386ca4b5feff4f9eebf9548a284ccf1d
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-12-24 17:51:41 +0700

    enable next.js image  config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const path = require("path");
 
 module.exports = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  images: {
+    domains: ["loremflickr.com"],
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
