35c373f5acb1f1a23c7305f8d20c486bc81b5ea6
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-12-25 19:57:22 +0700

    fix mongoose error timeout

--- utils/database/connectDB.js
+++ utils/database/connectDB.js
@@ -6,12 +6,17 @@ async function connectDB() {
 
   if (!dbUri) throw new Error("Invalid Database URL");
   try {
-    const conn = await mongoose.connect(dbUri);
+    const conn = await mongoose.connect(dbUri, {
+      keepAlive: true,
+      keepAliveInitialDelay: 300000,
+      serverSelectionTimeoutMS: 5000,
+    });
     console.log("[mongoose] Connected to DB");
     console.log(
       `[mongoose] connected on 'mongodb://*****:*****@${conn.connection.host}:${conn.connection.port}' `
     );
     console.log(`[mongoose] mongo database : ${conn.connection.name}`);
+    return conn;
   } catch (error) {
     console.log("could not connect to DB");
     console.log(error);
