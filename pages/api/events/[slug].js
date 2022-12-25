c2b82a5d8f90e27c4673bd24d228589a98809480
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-12-25 19:55:27 +0700

    event detail API endpoint

---
+++ pages/api/events/[slug].js
@@ -0,0 +1,32 @@
+import connectDB from "../../../utils/database/connectDB";
+import EventModel from "../../../utils/database/models/event.model";
+import { ToBaseError, resAPIError } from "../../../utils/error";
+
+async function postHandler(req, res) {
+  try {
+    res.status(201).json({ message: "successfully!" });
+  } catch (error) {
+    const err = ToBaseError(error);
+    return resAPIError(res, err);
+  }
+}
+
+async function getHandler(req, res) {
+  const _slug = req.query.slug;
+  try {
+    const event = await EventModel.findOne({ slug: _slug }).select("-__v");
+
+    res.status(200).json({ event });
+  } catch (error) {
+    const err = ToBaseError(error);
+    return resAPIError(res, err);
+  }
+}
+
+async function handler(req, res) {
+  await connectDB();
+  if (req.method === "POST") return postHandler(req, res);
+  if (req.method === "GET") return getHandler(req, res);
+}
+
+export default handler;
