b1d2da57b0a5f772de4bb5f4f51d77370ac8f4a0
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-12-29 18:22:34 +0700

    active class in header nav link

--- components/layout/MainHeader.js
+++ components/layout/MainHeader.js
@@ -1,7 +1,10 @@
 import { Col, Container, Nav, Row } from "react-bootstrap";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function MainHeader() {
+  const router = useRouter();
+
   return (
     <Container className="event-container border-bottom mb-3">
       <header className="header pt-4 pb-3">
@@ -15,7 +18,10 @@ function MainHeader() {
             <Nav className="header-nav justify-content-end align-items-center">
               <Nav.Item>
                 <Link href="/events" passHref>
-                  <Nav.Link className="header-nav-link text-link  ">
+                  <Nav.Link
+                    className="header-nav-link text-link  "
+                    active={router.pathname === "/events"}
+                  >
                     All Events
                   </Nav.Link>
                 </Link>
