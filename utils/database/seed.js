a641fab9c038b0dfc13560a6c53f8f3e24f5ff93
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-12-30 21:43:32 +0700

    add a function seed for generate users

--- utils/database/seed.js
+++ utils/database/seed.js
@@ -1,5 +1,7 @@
 import { faker } from "@faker-js/faker";
 import slug from "slug";
+import bcryptjs from "bcryptjs";
+import UserModel from "./models/user.model";
 
 function getPastDateInISOFormat(months, days = 0) {
   const pastDate = new Date();
@@ -8,16 +10,16 @@ function getPastDateInISOFormat(months, days = 0) {
   return pastDate.toISOString();
 }
 
-export function generateEvents(result = 10, withPast = false) {
+export async function generateEvents(result = 10, { includePast = false }) {
   let data = [];
-  for (let index = 0; index < result; index++) {
+  for (let _i = 0; _i < result; _i++) {
     const totalTitleLength = faker.datatype.number({ min: 8, max: 15 });
     const totalDescLength = faker.datatype.number({ min: 80, max: 200 });
     const title = faker.random.words(totalTitleLength);
     const desc = faker.random.words(totalDescLength);
     const location = faker.address.streetAddress(true);
 
-    const isPastDate = withPast ? faker.datatype.boolean() : false;
+    const isPastDate = includePast ? faker.datatype.boolean() : false;
     const dateWithPast = getPastDateInISOFormat(
       faker.datatype.number({ min: 1, max: 3 }),
       faker.datatype.number({ min: 1, max: 30 })
@@ -32,8 +34,14 @@ export function generateEvents(result = 10, withPast = false) {
     const image = faker.image.abstract(1920, 1280, true);
     const eventSlug = slug(title, "-");
     const isFeatured = faker.datatype.boolean();
+    const randomUser = await UserModel.aggregate([
+      { $match: { role: "USER" } },
+      { $sample: { size: 1 } },
+    ]);
     data.push({
       title,
+      user: randomUser[0]?._id,
+      status: "APPROVED",
       slug: eventSlug,
       description: desc,
       location,
@@ -44,5 +52,31 @@ export function generateEvents(result = 10, withPast = false) {
     });
   }
   console.log("RETURN EVENT GENERATE DATA");
+  return data;
+}
+
+export function generateUsers(result = 10) {
+  let data = [];
+  console.log("PREPARING TO GENERATE USERS DATA...");
+
+  for (let _i = 0; _i < result; _i++) {
+    const name1st = faker.name.firstName();
+    const name2nd = faker.name.lastName();
+    const name = `${name1st} ${name2nd}`;
+    const username = name.split(" ").join("_").toLowerCase();
+    const email = faker.internet.email(name1st, name2nd, "storegg.com");
+    const password = bcryptjs.hashSync("12345", 12);
+    const avatar = faker.image.avatar();
+
+    data.push({
+      name,
+      username,
+      email,
+      avatar,
+      password,
+    });
+  }
+
+  console.log("RETURN USERS DATA...");
   return data;
 }
