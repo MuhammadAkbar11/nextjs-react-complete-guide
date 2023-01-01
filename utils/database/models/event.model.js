c34a55809da8dd263c81db437889e5c0ca0a8e1a
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-01-01 21:56:56 +0700

    user reference in event schema

--- utils/database/models/event.model.js
+++ utils/database/models/event.model.js
@@ -1,34 +1,49 @@
 import { Schema, model, models } from "mongoose";
 
-const eventSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const eventSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    slug: { type: String },
+    description: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "UserModel",
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    dateStart: {
+      type: Date,
+      required: true,
+      index: true,
+    },
+    dateEnd: {
+      type: Date,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["REJECT", "PENDING", "APPROVED"],
+      default: "PENDING",
+    },
+    isFeatured: {
+      type: Boolean,
+    },
   },
-  slug: { type: String },
-  description: {
-    type: String,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  dateStart: {
-    type: Date,
-    required: true,
-  },
-  dateEnd: {
-    type: Date,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  isFeatured: {
-    type: Boolean,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const EventModel =
   models.EventModel || model("EventModel", eventSchema, "events");
